0ce4725dff842ed4e7385b5766f66267afd5cd42
annepham99 <s-phaman@bsd405.org>
2016-08-17 12:55:51 -0700

    Website Stuff

---
+++ feelgood.js
@@ -0,0 +1,51 @@
+function fionaImg(x) {
+    x.style.height = "23%";
+    x.style.width = "23%";
+}
+
+function MouseRollover(MyImage) {
+    MyImage.src = "geneva2.png";
+    MyImage.style.height = "25%";
+    MyImage.style.width = "25%";
+    }
+function MouseOut(MyImage) {
+    MyImage.src = "geneva.jpg";
+    MyImage.style.height = "20%";
+    MyImage.style.width = "20%";
+    }
+
+function MouseRolloverFiona(Fiona) {
+  Fiona.src = "fiona2.png";
+  Fiona.style.height = "28%";
+  Fiona.style.width = "28%";
+}
+
+function MouseOutFiona (Fiona) {
+  Fiona.src = "fiona.jpg";
+  Fiona.style.height = "23%";
+  Fiona.style.width = "23%";
+}
+
+function MouseRolloverAnne(Anne){
+  Anne.src = "anne2.png";
+  Anne.style.height = "25%";
+  Anne.style.width = "25%";
+}
+
+function MouseOutAnne(Anne){
+  Anne.src = "anne.jpg";
+  Anne.style.height = "20%";
+  Anne.style.width = "20%";
+}
+
+function MouseRolleoverNawaal(Nawaal){
+  Nawaal.src = "nawaal2.png";
+  Nawaal.style.height = "25%";
+  Nawaal.style.width = "25%";
+}
+
+function MouseOutNawaal(Nawaal){
+  Nawaal.src = "nawaal.png";
+  Nawaal.style.height = "20%";
+  Nawaal.style.width = "20%";
+}
