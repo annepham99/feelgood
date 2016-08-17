60123a997c59310793b4e5f283757f3cb22d3280
FionaHL <fofamoomis@gmail.com>
2016-08-17 08:55:45 -0700

    final over + running page?

---
+++ FINAL_HOVER.js
@@ -0,0 +1,13 @@
+function normalpic(x){
+    x.style.height="50";
+    x.style.width="50";
+}
+
+function makepicbigger(x){
+    x.style.height = "200px";
+    x.style.width = "200px";
+    /*var image = document.getElementById("image");
+    if (image.src.match("")){
+        image.src = 60%*/
+    }
+    
